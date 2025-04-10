1b39adc11db177c9cd6d4759d7bc7beffa48e1e8
AnaVSSCaldeira <anavsscaldeira@gmail.com>
2025-04-10 17:12:54 -0300

    Adicao do stun

--- protect-the-gems/Scenes/Game/Scripts/enemy_1.gd
+++ protect-the-gems/Scenes/Game/Scripts/enemy_1.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const speed = 100
 var attack = false
+var stun = false
 @onready var gem
 @onready var nav_agent = $NavigationAgent2D
 
@@ -10,15 +11,19 @@ func _ready():
 	make_path()
 
 func _physics_process(_delta):
-	var dir = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = dir * speed
-	move_and_slide()
+	if !stun:
+		var dir = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = dir * speed
+		move_and_slide()
 
 func make_path():
 	nav_agent.target_position = gem.global_position
 
-func _on_timer_timeout():
+func _on_timer_timeout(): #timer do navigation
 	make_path()
+
+func _on_stun_timer_timeout(): #timer do stun
+	stun = false
 
 func _on_area_2d_mouse_entered():
 	attack = true
@@ -29,5 +34,11 @@ func _on_area_2d_mouse_exited():
 func _input(event):
 	if event.is_action_pressed("Click") and attack == true:
 		$Life.value = $Life.value - int($"/root/Global".weapon_damage)
+		#stun
+		stun = true
+		$Stun_Timer.wait_time = $"/root/Global".stun_time
+		$Stun_Timer.start()
+
 	if $Life.value == 0:
 		queue_free()
+		
--- protect-the-gems/Scenes/global.gd
+++ protect-the-gems/Scenes/global.gd
@@ -2,8 +2,8 @@ extends Node
 
 #informacoes do player
 @export var current_weapon: CompressedTexture2D = load("res://icon.svg")
-@export var weapon_damage: int = 1
-@export var stun_chance: int = 5
+@export var weapon_damage: int = 2
+@export var stun_time: float = 0.5
 @export var money: int = 0
 
 #informacoes do jogo
